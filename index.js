cdfdce7ee4ea7ea2c8ecab17b1ee7ca01412f19e
𝗞𝗔𝗩𝗜-𝗘𝗫𝗘 𝗢𝗪𝗡𝗘𝗥 👨‍💻💗 <tharushankavishka02@gmail.com>
2025-03-09 08:58:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const ownerNumber = ['94760383959']
 
 
 const { exec } = require('child_process');
-const AdmZip = require('adm-zip'); // Import AdmZip for extraction
+const AdmZip = require('adm-zip');
 
 //=========================dl-ZIP========================
 const PLUGINS_DIR = "./plugins/"; // Directory where plugins will be extracted
